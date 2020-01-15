35d174b681b5f8a917cb7d9aef60d802a6fcd444
chuyingjie <chuyingjie@163.com>
2020-01-15 16:31:43 +0800

    qqqq

---
+++ gitdemo/src/com/itcast/FirstDemo.java
@@ -0,0 +1,11 @@
+package com.itcast;
+
+/**
+ * @author shkstart
+ * @create 2020-01-15 16:26
+ */
+public class FirstDemo {
+    public static void main(String[] args) {
+        System.out.println("你好 git");
+    }
+}
