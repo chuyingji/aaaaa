ba1c9b4efcd2cc2a73498deacf3254dbd003bc57
chuyingjie <chuyingjie@163.com>
2020-01-15 16:33:45 +0800

    本次修改

--- gitdemo/src/com/itcast/FirstDemo.java
+++ gitdemo/src/com/itcast/FirstDemo.java
@@ -8,4 +8,9 @@ public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("你好 git");
     }
+    public void test(){
+        int i= 10 ;
+        i=i+222;
+        System.out.println(i);
+    }
 }
